fcecf3579ea5f1c942dab68dc132f6ad3a040e3a
Adam Klein <adam@500tech.com>
2019-11-24 18:00:40 +0200

    Fixed jokes

--- src/jokes.js
+++ src/jokes.js
@@ -3,12 +3,12 @@ const quotes = [
   'Because he used up all his cache',
   'How do you comfort a JavaScript bug?',
   'You console it',
-  `Did you hear about the restaurant on the moon?',
-  'Great food, no atmosphere.`,
-  `Want to hear a joke about paper?',
-  'Nevermind it's tearable.`,
-  `What do you call a man with a rubber toe?',
-  'Roberto.`
+  'Did you hear about the restaurant on the moon?',
+  'Great food, no atmosphere.',
+  'Want to hear a joke about paper?',
+  'Nevermind it\'s tearable.',
+  'What do you call a man with a rubber toe?',
+  'Roberto.'
 ];
 let index = 0;
 export function generateJoke() {
